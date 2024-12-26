ce766c349fc63d80b0b83d27ec8b5b80efa1acd9
Your Name <you@example.com>
2024-12-26 11:00:16 +0300

    Fix client initialization and state management

--- socket.js
+++ socket.js
@@ -10,6 +10,9 @@ var MPPClient = function() {
     this.noteBuffer = [];
     this.noteBufferTime = 0;
     this.noteFlushInterval = null;
+    this._events = {};
+    
+    var self = this;
     
     // Initialize socket with correct configuration
     this.socket = io(window.location.origin, {
@@ -22,18 +25,22 @@ var MPPClient = function() {
         timeout: 30000
     });
 
-    // Add connection error handling
-    this.socket.on('connect_error', function(error) {
-        console.error('Connection error:', error);
-        self.emit('connect_error', error);
-    });
-
-    this.socket.on('connect_timeout', function(timeout) {
-        console.error('Connection timeout:', timeout);
-        self.emit('connect_timeout', timeout);
-    });
-
     this.bindEventListeners();
+
+    // Initialize note flush interval
+    this.noteFlushInterval = setInterval(function() {
+        if(self.noteBuffer.length === 0) return;
+        
+        var timeDiff = Date.now() - self.noteBufferTime;
+        if(timeDiff >= 200) { // Flush every 200ms
+            self.socket.emit('n', {
+                t: self.noteBufferTime,
+                n: self.noteBuffer,
+                room: self.channel
+            });
+            self.noteBuffer = [];
+        }
+    }, 200);
 };
 
 MPPClient.prototype.bindEventListeners = function() {
@@ -45,15 +52,24 @@ MPPClient.prototype.bindEventListeners = function() {
         self.canConnect = true;
         self.participantId = self.socket.id;
         
+        // Initialize own participant
+        self.participants[self.participantId] = {
+            id: self.participantId,
+            name: "Anonymous",
+            color: "#fff",
+            x: 50,
+            y: 50
+        };
+        
         // Start ping interval
         if(self.pingInterval) clearInterval(self.pingInterval);
         self.pingInterval = setInterval(function() {
             self.socket.emit('t1', Date.now());
         }, 20000);
         
-        // Join channel
+        // Join channel if one is set
         if(self.channel) {
-            self.socket.emit('ch', {_id: self.channel._id});
+            self.setChannel(self.channel);
         }
 
         self.emit('connect');
@@ -116,15 +132,23 @@ MPPClient.prototype.bindEventListeners = function() {
     });
 };
 
-MPPClient.prototype.setChannel = function(id, settings) {
-    if(!this.canConnect) return;
-    if(typeof id !== 'string') return;
-    if(!settings) settings = {};
-    var channel = {_id: id, settings: settings};
-    this.channel = channel;
-    if(this.socket && this.socket.connected) {
-        this.socket.emit('ch', {_id: id, set: settings});
-    }
+MPPClient.prototype.setChannel = function(channelId, callback) {
+    if(!this.canConnect || !channelId) return;
+    
+    var self = this;
+    this.channel = channelId;
+    this.socket.emit('ch', {_id: channelId}, function(err, data) {
+        if(err) {
+            console.error(err);
+            if(callback) callback(err);
+            return;
+        }
+        self.participants = {};
+        if(data.p) data.p.forEach(function(p) {
+            self.participants[p.id] = p;
+        });
+        if(callback) callback(null, data);
+    });
 };
 
 MPPClient.prototype.sendArray = function(arr) {
@@ -139,24 +163,14 @@ MPPClient.prototype.isOwner = function() {
 };
 
 MPPClient.prototype.preventsPlaying = function() {
-    return this.channel && this.channel.settings.crownsolo && !this.isOwner();
+    return !this.canConnect || !this.channel || !this.participantId;
 };
 
 MPPClient.prototype.startNote = function(note, vel) {
-    if(!this.canConnect || !this.socket.connected) return;
-    
-    var msg = {
-        n: note,
-        v: vel,
-        room: this.channel._id
-    };
-
-    // Send note immediately
-    this.socket.emit('n', {
-        t: Date.now(),
-        n: [msg],
-        room: this.channel._id
-    });
+    if(this.noteBuffer.length === 0) {
+        this.noteBufferTime = Date.now();
+        this.noteBuffer.push({n: note, v: vel});
+    }
 };
 
 MPPClient.prototype.stopNote = function(note) {
@@ -178,7 +192,6 @@ MPPClient.prototype.stopNote = function(note) {
 
 // EventEmitter implementation
 MPPClient.prototype.on = function(event, callback) {
-    if(!this._events) this._events = {};
     if(!this._events[event]) this._events[event] = [];
     this._events[event].push(callback);
 };
@@ -191,10 +204,128 @@ MPPClient.prototype.off = function(event, callback) {
 };
 
 MPPClient.prototype.emit = function(event) {
-    if(!this._events) this._events = {};
     if(!this._events[event]) return;
     var args = Array.prototype.slice.call(arguments, 1);
     for(var i = 0; i < this._events[event].length; i++) {
         this._events[event][i].apply(this, args);
     }
+};
+
+MPPClient.prototype.getOwnParticipant = function() {
+    return this.participants[this.participantId];
+};
+
+MPPClient.prototype.startNote = function(note, vel) {
+    if(this.noteBuffer.length === 0) {
+        this.noteBufferTime = Date.now();
+        this.noteBuffer.push({n: note, v: vel});
+    }
+};
+
+MPPClient.prototype.setChannel = function(channelId, callback) {
+    if(!this.canConnect || !channelId) return;
+    
+    var self = this;
+    this.channel = channelId;
+    this.socket.emit('ch', {_id: channelId}, function(err, data) {
+        if(err) {
+            console.error(err);
+            if(callback) callback(err);
+            return;
+        }
+        self.participants = {};
+        if(data.p) data.p.forEach(function(p) {
+            self.participants[p.id] = p;
+        });
+        if(callback) callback(null, data);
+    });
+};
+
+MPPClient.prototype.bindEventListeners = function() {
+    var self = this;
+
+    this.socket.on('connect', function() {
+        console.log('Connected to server');
+        self.connectionTime = Date.now();
+        self.canConnect = true;
+        self.participantId = self.socket.id;
+        
+        // Initialize own participant
+        self.participants[self.participantId] = {
+            id: self.participantId,
+            name: "Anonymous",
+            color: "#fff",
+            x: 50,
+            y: 50
+        };
+        
+        // Start ping interval
+        if(self.pingInterval) clearInterval(self.pingInterval);
+        self.pingInterval = setInterval(function() {
+            self.socket.emit('t1', Date.now());
+        }, 20000);
+        
+        // Join channel if one is set
+        if(self.channel) {
+            self.setChannel(self.channel);
+        }
+
+        self.emit('connect');
+    });
+
+    this.socket.on('disconnect', function() {
+        console.log('Disconnected from server');
+        self.participantId = null;
+        self.canConnect = false;
+        if(self.pingInterval) clearInterval(self.pingInterval);
+        if(self.noteFlushInterval) clearInterval(self.noteFlushInterval);
+        self.emit('disconnect');
+    });
+
+    this.socket.on('t2', function(t1) {
+        self.serverTimeOffset = Date.now() - t1;
+    });
+
+    this.socket.on('n', function(msg) {
+        self.emit('n', msg);
+    });
+
+    this.socket.on('ch', function(msg) {
+        self.channel = msg.ch;
+        self.emit('ch', msg);
+    });
+
+    this.socket.on('p', function(msg) {
+        self.participants[msg.id] = msg;
+        self.emit('participant added', msg);
+    });
+
+    this.socket.on('bye', function(id) {
+        var part = self.participants[id];
+        if(part) {
+            delete self.participants[id];
+            self.emit('participant removed', part);
+        }
+    });
+
+    // Handle status updates
+    this.socket.on('status', function(msg) {
+        self.emit('status', msg);
+    });
+
+    // Handle count updates
+    this.socket.on('count', function(msg) {
+        self.emit('count', msg);
+    });
+
+    // Handle chat messages
+    this.socket.on('a', function(msg) {
+        self.emit('a', msg);
+    });
+
+    // Handle errors
+    this.socket.on('error', function(err) {
+        console.error('Socket error:', err);
+        self.emit('error', err);
+    });
 }; 
